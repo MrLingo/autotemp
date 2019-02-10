dfafb9f656c6245161293fa7b33fd38b50bd9ad1
Lyubomir Mihaylov <l.mihaylov@abv.bg>
2019-02-10 16:28:10 +0200

    Update generators.py

--- generators.py
+++ generators.py
@@ -14,11 +14,9 @@ if __name__ != '__main__':
                                             tag_list[3], tag_list[4], tag_list[5],
                                             tag_list[6], tag_list[7], tag_list[8],
                                             tag_list[9], tag_list[10], tag_list[11])
-
         if not os.path.exists(html_path):
             os.makedirs(html_path)
         os.chdir(html_path)
-
         for x in range(num_of_files):
             html_doc = open('generated_html_doc_' + str(x) + '.html', 'w')
             html_doc.write(final_result)
@@ -38,11 +36,9 @@ if __name__ != '__main__':
                                               css_blueprint_list[2], css_blueprint_list[3],
                                               css_blueprint_list[4], css_blueprint_list[5],
                                               css_blueprint_list[6])
-
         if not os.path.exists(css_path):
             os.makedirs(css_path)
         os.chdir(css_path)
-
         css_doc = open('generated_css_sheet.css', 'w')
         css_doc.write(final_result)
         css_doc.close()
@@ -52,7 +48,6 @@ if __name__ != '__main__':
         if not os.path.exists(js_path):
             os.makedirs(js_path)
         os.chdir(js_path)
-
         for j in range(num_of_files):
             js_script = open('generated_js_script_' + str(j) + '.js', 'w')
             js_script.write("//Generated blank JS file.")
